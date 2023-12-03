c61abe26640fe10861be38a65e3f76e01de7b601
Gepardgame <thomas.degg@gmail.com>
2023-12-03 15:09:59 +0000

    First half of 2.

---
+++ day2/code.py
@@ -0,0 +1,8 @@
+import re
+text = open("input.txt", "r").read().split("\n")[:-1]
+blue = 14
+red = 12
+green = 13
+cubes = [{"id": int(re.findall(r"(?<=^Game\s)\d+", i)[0]), "blue": [int(x) for x in re.findall(r"\d+(?=\sblue)", i)], "red": [int(x) for x in re.findall(r"\d+(?=\sred)", i)], "green":  [int(x) for x in re.findall(r"\d+(?=\sgreen)", i)]}   for i in text ]
+sum1 = [ i["id"] for i in cubes if max(i["blue"]) <= blue and max(i["red"]) <= red and max(i["green"]) <= green ]
+print(sum(sum1))

5019e4bef96469358d3384ea9203e0ce429dff6b
Gepardgame <thomas.degg@gmail.com>
2023-12-04 20:17:52 +0000

    finished day4

--- day1/code.py
+++ day1/code.py
@@ -1,21 +1,26 @@
 import re
-text = open("input.txt", "r").read().split("\n")
-#digits1 = [ re.findall(r"\d", i) for i in text ]
-#sum1 = [ int(i[0] + i[-1]) for i in digits1 ]
-#print(sum(sum1))
-
+text = open("input1.txt", "r").read().split("\n")
+digits1 = [ re.findall(r"\d", i) for i in text ]
+sum1 = [ int(i[0] + i[-1]) for i in digits1 ]
+print(sum(sum1))
+"""text = ["two1nine",
+"eightwothree",
+"abcone2threexyz",
+"xtwone3four",
+"4nineeightseven2",
+"zoneight234",
+"7pqrstsixteen"]"""
 digits2 = [ re.findall(r"\d|one|two|three|four|five|six|seven|eight|nine", i) for i in text ]
 numbers = { "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9" }
-sum2 = [ int(numbers.get(i[0], i[0]) + numbers.get(i[-1], i[-1])) for i in digits2 ]
+#sum2 = [ int(numbers.get(i[0], i[0]) + numbers.get(i[-1], i[-1])) for i in digits2 ]
+sum2 = []
+for i in digits2:
+    #print(i)
+    first_digit = numbers.get(i[0], i[0])
+    last_digit = numbers.get(i[-1], i[-1])
+    current_sum = int(first_digit + last_digit)
+    #print("f: ", first_digit, "l: ", last_digit, "s: ", current_sum)
+    sum2.append(current_sum)
 print(sum2[-10:])
 print(sum(sum2))
 
-numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-
-def convert_to_digit(match):
-    return numbers.get(match.group(0), match.group(0))
-
-to_numbers = [re.sub(r"\d|one|two|three|four|five|six|seven|eight|nine", convert_to_digit, i) for i in text]
-digits2 = [ re.findall(r"\d", i) for i in to_numbers ]
-calibration_values = [int(line[0] + line[-1]) for line in digits2]
-print(sum(calibration_values))
--- day4/code.py
+++ day4/code.py
@@ -1,12 +1,11 @@
 import regex as re
 text = open("input.txt", "r").read().split("\n")
-numbers1 = [ ( re.findall(r"(?<=Card\s+)\d+(?=:)", i),  re.findall(r"\b\d+\b(?!:)(?=.+\|)", i), re.findall(r"(?<=\|.+)\b\d+\b", i) ) for i in text ]
-sum1 = [ int(2**(len(set(i).intersection(j))-1)) for _, i, j in numbers1 ]
+numbers1 = [ ( re.findall(r"\b\d+\b(?!:)(?=.+\|)", i), re.findall(r"(?<=\|.+)\b\d+\b", i) ) for i in text ]
+sum1 = [ int(2**(len(set(i).intersection(j))-1)) for i, j in numbers1 ]
 print(sum(sum1))
-
-numbers2 = [ (k, len(set(i).intersection(j))) for k, i, j in numbers1 ]
-for i, j in numbers2:
-    
-
-
-#x = [ [ for k in range(i, i+j ] for i, j in numbers2 ]
+t = [ 1 ] * len(text)
+numbers2 = [ len(set(i).intersection(j)) for i, j in numbers1 ]
+for i, j in enumerate(numbers2):
+    for k in range(i+1, i+j+1):
+        t[k] += t[i]
+print(sum(t))
